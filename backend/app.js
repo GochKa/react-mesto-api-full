f61bee2cdf7eb32f74a6fe09c62573964817700e
GochKa <gocha.workout@gmail.com>
2022-06-21 14:52:49 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,32 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-// CORS
-const allowedCors = [
-  'http://mestogram.gocha.nomoreparties.sbs',
-  'https://mestogram.gocha.nomoreparties.sbs',
-  'http://localhost:3005',
-  'https://localhost:3005',
-];
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-};
-
-app.use(allowedCors);
 
 // Логин
 app.post('/signin', celebrate({
