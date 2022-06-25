81cba902c48799875d7a9802000219b943e89dad
GochKa <gocha.workout@gmail.com>
2022-06-26 01:40:08 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 // const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
-// const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/user');
 const auth = require('./middlewares/auth');
 const NotFound = require('./errors/NotFound');
 
-const { PORT = 3006 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -18,42 +18,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 // app.use(cookieParser());
 app.use(requestLogger);
-const allowedCors = [
-  'http://localhost:3006',
-  'https://localhost:3006',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'https://mestogram.gocha.nomoreparties.sbs',
-  'http://mestogram.gocha.nomoreparties.sbs',
-];
+app.use(cors());
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', allowedCors);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
-
-app.post('/sign-in', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/sign-up', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -67,6 +34,12 @@ app.post('/sign-up', celebrate({
     }),
   }),
 }), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 app.use(auth);
 app.use('/users', require('./routes/user'));
 app.use('/cards', require('./routes/card'));
@@ -92,7 +65,4 @@ app.use((err, _, res, next) => {
   res.status(500).send({ message: 'Ошибка по умолчанию.' });
   next();
 });
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Приложение запущено на порту ${PORT}`);
-});
+app.listen(PORT);
