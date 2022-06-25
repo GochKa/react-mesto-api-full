f4a1982a79ef4b3fc86571698aa471a7390865f5
GochKa <gocha.workout@gmail.com>
2022-06-26 02:17:25 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // app.use(cookieParser());
 app.use(requestLogger);
 
-app.post('/signup', celebrate({
+app.post('/sign-up', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -33,7 +33,7 @@ app.post('/signup', celebrate({
     }),
   }),
 }), createUser);
-app.post('/signin', celebrate({
+app.post('/sign-in', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
